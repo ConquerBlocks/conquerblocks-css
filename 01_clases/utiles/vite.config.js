5625f82ae0a8626b0d2ebd68d9a0712e3796409f
Bienvenido Sáez Muelas <bienvenidosaez@gmail.com>
2024-09-29 23:43:10 +0200

    Update vite.config.js
    añadiendo créditos ;D

--- 01_clases/utiles/vite.config.js
+++ 01_clases/utiles/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   server: {
-    host: true, // Añadiendo host:true para poder acceder desde cualquier dispositivo de tu red, como por ejemplo para probar el responsive desde tu teléfono o tu tablet conectado a la misma Wifi/red
+    host: true, // Añadiendo host:true para poder acceder desde cualquier dispositivo de tu red, como por ejemplo para probar el responsive desde tu teléfono o tu tablet conectado a la misma Wifi/red. By @Eleazar
     watch: {
       usePolling: true, // Recomendado para WSL, permite detectar cambios en archivos
     },
